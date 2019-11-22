df10864f0abfcce6de15f38d2ccc0eda7eae34ac
Andrew Charneski <acharneski@gmail.com>
2019-11-21 21:58:19 -0800

    Various updates

---
+++ autocoder/src/main/java/com/simiacryptus/devutil/ops/InsertGCs.java
@@ -0,0 +1,48 @@
+package com.simiacryptus.devutil.ops;
+
+import com.simiacryptus.devutil.AutoCoder;
+import org.eclipse.jdt.core.dom.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class InsertGCs extends FileAstVisitor {
+
+  public InsertGCs(CompilationUnit cu, File file) {
+    super(cu, file);
+  }
+
+  @Override
+  public void endVisit(MethodDeclaration node) {
+    final AST ast = node.getAST();
+    final Block body = node.getBody();
+    if(!node.isConstructor()) {
+      body.statements().add(0, newGCCall(ast));
+    }
+    if(null != body && body.statements().size() > 0) {
+      final ArrayList<ReturnStatement> returnStatements = new ArrayList<>();
+      body.accept(new ASTVisitor() {
+        @Override
+        public void endVisit(ReturnStatement node) {
+          final Block block = ast.newBlock();
+          block.statements().add(newGCCall(ast));
+          block.statements().add(ASTNode.copySubtree(ast, node));
+          AutoCoder.replace(node, block);
+          returnStatements.add(node);
+        }
+      });
+      if(returnStatements.isEmpty()) {
+        body.statements().add(newGCCall(ast));
+      }
+    }
+  }
+
+  @NotNull
+  public ASTNode newGCCall(AST ast) {
+    final MethodInvocation methodInvocation = ast.newMethodInvocation();
+    methodInvocation.setExpression(ast.newSimpleName("System"));
+    methodInvocation.setName(ast.newSimpleName("gc"));
+    return ast.newExpressionStatement(methodInvocation);
+  }
+}
---
+++ autocoder/src/main/java/com/simiacryptus/devutil/ops/RemoveGC.java
@@ -0,0 +1,28 @@
+package com.simiacryptus.devutil.ops;
+
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+class RemoveGC extends FileAstVisitor {
+
+  RemoveGC(CompilationUnit compilationUnit, File file) {
+    super(compilationUnit, file);
+  }
+
+  @Override
+  public void endVisit(@NotNull MethodInvocation node) {
+    final Expression expression = node.getExpression();
+    if(null==expression) return;
+    final ITypeBinding typeBinding = expression.resolveTypeBinding();
+    final String binaryName = typeBinding.getBinaryName();
+    if (null != binaryName && binaryName.equals(System.class.getCanonicalName())) {
+      node.getParent().delete();
+    }
+  }
+
+}
